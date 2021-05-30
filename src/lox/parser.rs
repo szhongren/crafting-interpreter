6a5f28bc18a756c4d88d105656f9076e5dba15ce
Zhongren Shao <shao.zhongren@gmail.com>
2021-05-29 21:08:00 -0500

    push up error if error

--- src/lox/parser.rs
+++ src/lox/parser.rs
@@ -102,9 +102,9 @@ impl<'a> Parser<'a> {
 
     fn if_statement(&self) -> Result<Stmt, String> {
         // ifStatement    → "if" "(" expression ")" statement ( "else" statement )?;
-        self.consume(TokenType::LeftParen, "Expect '(' after 'if'.");
+        self.consume(TokenType::LeftParen, "Expect '(' after 'if'.")?;
         let condition = self.expression()?;
-        self.consume(TokenType::RightParen, "Expect ')' after 'if' condition.");
+        self.consume(TokenType::RightParen, "Expect ')' after 'if' condition.")?;
 
         let then_branch = self.statement()?;
 
