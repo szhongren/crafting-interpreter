88f190d031b5b57113b5426ec523fa212a04ebe5
Zhongren Shao <shao.zhongren@gmail.com>
2021-06-11 23:10:33 -0500

    added This Expr

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ fn main() -> Result<()> {
                         "Literal: Object value",
                         "Logical: Expr left, Token operator, Expr right",
                         "Set: Expr object, Token name, Expr value",
+                        "This: Token keyword",
                         "Urnary: Token operator, Expr right",
                         "Variable: Token name",
                     ],
